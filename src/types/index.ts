7bab33b9c592e51c6ccbcf8deb6cfa4ba6e92fe3
saymyname-7 <samhoho998@gmail.com>
2023-07-18 18:27:19 +0800

    fix optional ts syntax

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,13 @@
 export interface ICreateQuizProps {
-  id: string?
-  title: string
-  codeLanguageId: number
-  difficultyLevelId: number
-  userId: string
-  instruction: string
-  answer: string
-  defaultCode: string
-  locale: string
+  id?: string;
+  title: string;
+  codeLanguageId: number;
+  difficultyLevelId: number;
+  userId: string;
+  instruction: string;
+  answer: string;
+  defaultCode: string;
+  locale: string;
 }
 
 export interface ICreateQuizSolutionProps {
