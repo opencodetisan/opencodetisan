4c33abfd6cc392052bd49fd9562f074d47e815a1
saymyname-7 <samhoho998@gmail.com>
2023-07-18 10:49:16 +0800

    temporary code formatting

--- src/lib/core/__test__/quiz.test.ts
+++ src/lib/core/__test__/quiz.test.ts
@@ -1,9 +1,9 @@
-import { createQuiz } from '@/lib/core/quiz'
-import { faker } from '@faker-js/faker'
-import { prismaMock } from '@/lib/db/prisma-mock-singleton'
+import { createQuiz } from "@/lib/core/quiz";
+import { faker } from "@faker-js/faker";
+import { prismaMock } from "@/lib/db/prisma-mock-singleton";
 
-describe('Quiz module', () => {
-  test('createQuiz function should save the quiz data and return the saved data', async () => {
+describe("Quiz module", () => {
+  test("createQuiz function should save the quiz data and return the saved data", async () => {
     const quizData = {
       id: faker.string.uuid(),
       userId: faker.string.uuid(),
@@ -13,14 +13,14 @@ describe('Quiz module', () => {
       instruction: faker.lorem.paragraphs(),
       answer: faker.lorem.paragraphs(),
       defaultCode: faker.lorem.paragraphs(),
-      locale: 'en',
-    }
-    prismaMock.quiz.create.mockResolvedValue(quizData)
-    const savedQuiz = await createQuiz( quizData)
-    expect(savedQuiz).toBe(quizData)
-  })
+      locale: "en",
+    };
+    prismaMock.quiz.create.mockResolvedValue(quizData);
+    const savedQuiz = await createQuiz(quizData);
+    expect(savedQuiz).toBe(quizData);
+  });
 
-  test('Missing title parameter should raise an missing title error', async () => {
+  test("Missing title parameter should raise an missing title error", async () => {
     const quizData = {
       id: faker.string.uuid(),
       userId: faker.string.uuid(),
@@ -30,11 +30,13 @@ describe('Quiz module', () => {
       instruction: faker.lorem.paragraphs(),
       answer: faker.lorem.paragraphs(),
       defaultCode: faker.lorem.paragraphs(),
-      locale: 'en',
-    }
-    prismaMock.quiz.create.mockResolvedValue(quizData)
-    expect( async () =>  await createQuiz(quizData)).rejects.toThrow('missing quiz title')
-  })
+      locale: "en",
+    };
+    prismaMock.quiz.create.mockResolvedValue(quizData);
+    expect(async () => await createQuiz(quizData)).rejects.toThrow(
+      "missing quiz title",
+    );
+  });
 
   //Continue to test remaining missing mandatory input parameter for createQuiz function
-})
+});
