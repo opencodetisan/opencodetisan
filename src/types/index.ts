d7f6af5e8abbad1abfbe2a5e2e139a5e33d609d9
“MC” <139092715+iamcodetisan@users.noreply.github.com>
2023-07-18 15:14:18 +0800

    Fix type error that causing build error

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,11 @@
 export interface ICreateQuizProps {
-  id: string?
-  title: string
-  codeLanguageId: number
-  difficultyLevelId: number
-  userId: string
-  instruction: string
-  answer: string
-  defaultCode: string
-  locale: string
+  id: string;
+  title: string;
+  codeLanguageId: number;
+  difficultyLevelId: number;
+  userId: string;
+  instruction: string;
+  answer: string;
+  defaultCode: string;
+  locale: string;
 }
