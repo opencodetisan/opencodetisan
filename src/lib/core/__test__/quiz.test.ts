05f17816ef1e51072b6fc6f8ae65861392201d75
saymyname-7 <samhoho998@gmail.com>
2023-07-19 21:47:30 +0800

    add missing codeLanguageId param testcase

--- src/lib/core/__test__/quiz.test.ts
+++ src/lib/core/__test__/quiz.test.ts
@@ -43,6 +43,23 @@ describe("Quiz module", () => {
   });
 
   //Continue to test remaining missing mandatory input parameter for createQuiz function
+  test("Missing codeLanguageId parameter should raise an missing code language error", async () => {});
+  const quizData = {
+    id: faker.string.uuid(),
+    userId: faker.string.uuid(),
+    title: faker.lorem.text(),
+    // codeLanguageId: 1,
+    difficultyLevelId: 1,
+    instruction: faker.lorem.paragraphs(),
+    answer: faker.lorem.paragraphs(),
+    defaultCode: faker.lorem.paragraphs(),
+    locale: "en",
+  };
+  prismaMock.quiz.create.mockResolvedValue(quizData);
+  expect(async () => await createQuiz(quizData)).rejects.toThrow(
+    "missing code language id",
+  );
+
   test("createQuizSolution fn should save and return quiz solution", async () => {
     const solutionData = {
       quizId: faker.string.uuid(),
