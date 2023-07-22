c406a326658d02b6bab861b30f473e925266e08b
saymyname-7 <samhoho998@gmail.com>
2023-07-22 16:07:44 +0800

    test createCandidateQuizSubmission fn return val

---
+++ src/lib/core/__test__/candidate.test.ts
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+import { prismaMock } from "../../db/prisma-mock-singleton";
+import { createCandidateQuizSubmission } from "../candidate";
+
+describe("Candidate module", () => {
+  test(" saveUserQuizSubmission fn should save and return the submission data", async () => {
+    const submissionData: any = {
+      userId: faker.string.uuid(),
+      quizId: faker.string.uuid(),
+      code: faker.lorem.text(),
+    };
+    prismaMock.submission.create.mockResolvedValue(submissionData);
+    expect(await createCandidateQuizSubmission(submissionData)).toEqual(
+      submissionData,
+    );
+  });
+});
