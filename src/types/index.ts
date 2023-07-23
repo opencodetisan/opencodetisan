bedb79b2916350f83a5b5a9e770226940fed3bb9
saymyname-7 <samhoho998@gmail.com>
2023-07-23 16:05:41 +0800

    code formatting /types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,47 +1,47 @@
 export interface ICreateQuizProps {
-  id: string;
-  title: string;
-  codeLanguageId: number;
-  difficultyLevelId: number;
-  userId: string;
-  instruction: string;
-  answer: string;
-  defaultCode: string;
-  locale: string;
+  id: string
+  title: string
+  codeLanguageId: number
+  difficultyLevelId: number
+  userId: string
+  instruction: string
+  answer: string
+  defaultCode: string
+  locale: string
 }
 
 export interface ICreateQuizSolutionProps {
-  quizId: string;
-  code: string;
+  quizId: string
+  code: string
   //entryFunction: mainFunction,
   //appendCode,
-  sequence: number;
-  importDirectives: string;
-  testRunner: string;
+  sequence: number
+  importDirectives: string
+  testRunner: string
 }
 
 export interface ICreateQuizTestCaseProps {
-  solutionId: string;
-  input: string;
-  output: string;
-  sequence: number;
+  solutionId: string
+  input: string
+  output: string
+  sequence: number
 }
 
 export interface IUpdateQuizDataProps {
-  id: string;
-  userId: string;
-  instruction?: string;
-  title?: string;
-  codeLanguageId?: number;
-  difficultyLevelId?: number;
-  postUrl?: string;
+  id: string
+  userId: string
+  instruction?: string
+  title?: string
+  codeLanguageId?: number
+  difficultyLevelId?: number
+  postUrl?: string
 }
 
 export interface IUpdateQuizSolutionProps {
-  solutionId?: string;
-  quizId: string;
-  code: string;
-  importDirectives?: string;
-  testRunner?: string;
-  defaultCode?: string;
+  solutionId?: string
+  quizId: string
+  code: string
+  importDirectives?: string
+  testRunner?: string
+  defaultCode?: string
 }
