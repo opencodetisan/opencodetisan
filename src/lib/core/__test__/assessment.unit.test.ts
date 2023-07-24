3b3775f297472d73284eac971f0eda887a0710bf
saymyname-7 <samhoho998@gmail.com>
2023-07-24 23:26:03 +0800

    test createAssessmentCandidateEmails missing assessmentId err

--- src/lib/core/__test__/assessment.unit.test.ts
+++ src/lib/core/__test__/assessment.unit.test.ts
@@ -36,4 +36,27 @@ describe('Assessment module', () => {
       candidateEmails,
     )
   })
+
+  test('missing assessmentId parameter should return a missing assessmentId error', async () => {
+    const candidateEmails: any = [
+      {
+        assessmentId: faker.string.uuid(),
+        email: faker.lorem.text(),
+        statusCode: 200,
+        errorMessage: faker.lorem.text(),
+      },
+      {
+        // assessmentId: faker.string.uuid(),
+        email: faker.lorem.text(),
+        statusCode: 200,
+        errorMessage: faker.lorem.text(),
+      },
+    ]
+    prismaMock.assessmentCandidateEmail.createMany.mockResolvedValue(
+      candidateEmails,
+    )
+    expect(
+      async () => await createAssessmentCandidateEmails(candidateEmails),
+    ).rejects.toThrow('missing assessmentId')
+  })
 })
