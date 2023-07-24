c96362cc8d4eb8e5e035efbf0f1dafc16432607b
saymyname-7 <samhoho998@gmail.com>
2023-07-24 23:23:39 +0800

    test createAssessmentCandidateEmails return val

--- src/lib/core/__test__/assessment.unit.test.ts
+++ src/lib/core/__test__/assessment.unit.test.ts
@@ -1,6 +1,6 @@
 import {faker} from '@faker-js/faker'
 import {prismaMock} from '../../db/prisma-mock-singleton'
-import {createAssessment} from '../assessment'
+import {createAssessment, createAssessmentCandidateEmails} from '../assessment'
 
 describe('Assessment module', () => {
   test(' saveUserQuizSubmission fn should save and return the submission data', async () => {
@@ -12,5 +12,28 @@ describe('Assessment module', () => {
     }
     prismaMock.assessment.create.mockResolvedValue(assessmentData)
     expect(await createAssessment(assessmentData)).toEqual(assessmentData)
+  })
+
+  test('createAssessmentCandidateEmails fn should save and return the count number', async () => {
+    const candidateEmails: any = [
+      {
+        assessmentId: faker.string.uuid(),
+        email: faker.lorem.text(),
+        statusCode: 200,
+        errorMessage: faker.lorem.text(),
+      },
+      {
+        assessmentId: faker.string.uuid(),
+        email: faker.lorem.text(),
+        statusCode: 200,
+        errorMessage: faker.lorem.text(),
+      },
+    ]
+    prismaMock.assessmentCandidateEmail.createMany.mockResolvedValue(
+      candidateEmails,
+    )
+    expect(await createAssessmentCandidateEmails(candidateEmails)).toEqual(
+      candidateEmails,
+    )
   })
 })
