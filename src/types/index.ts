d0d0a30d204e05e94bdd4667fde30255702a4cab
saymyname-7 <samhoho998@gmail.com>
2023-07-26 13:21:45 +0800

    add ITestCaseClientProps

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,6 +8,11 @@ export interface ITestCaseProps {
   solutionId: string
 }
 
+export interface ITestCaseClientProps {
+  input: string[]
+  output: string[]
+}
+
 export interface ICreateQuizProps {
   id: string
   title: string
