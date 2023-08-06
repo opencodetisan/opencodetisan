2bf4fa96360f8c555057488fe82bc6df655a29a1
saymyname-7 <samhoho998@gmail.com>
2023-08-06 20:26:40 +0800

    add compressJsonStr fn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import {compressSync, strToU8} from 'fflate'
+
 export const convertToMinuteSecond = (seconds: number) => {
   if (!seconds) {
     throw Error('missing seconds')
@@ -5,4 +7,13 @@ export const convertToMinuteSecond = (seconds: number) => {
   const minutes = Math.floor(seconds / 60)
   const newSeconds = Math.round(seconds - minutes * 60)
   return {minutes, seconds: newSeconds}
+}
+
+export const compressJsonStr = (jsonStr: string) => {
+  if (!jsonStr) {
+    throw Error('missing data')
+  }
+  const buf = strToU8(jsonStr)
+  const compressed = compressSync(buf, {level: 6, mem: 8})
+  return compressed
 }
