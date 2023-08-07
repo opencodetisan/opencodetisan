edaab5f97d591ee5a99674c595f556f8107dd77d
saymyname-7 <samhoho998@gmail.com>
2023-08-07 13:33:22 +0800

    add saveSessionReplayService fn

---
+++ src/lib/core/service.ts
@@ -0,0 +1,20 @@
+import {compressJsonStr, createDir, writeToLocal} from '../utils'
+
+export const saveSessionReplayService = ({
+  data,
+  userId,
+  assessmentQuizSubId,
+}: {
+  data: any
+  userId: string
+  assessmentQuizSubId: string
+}) => {
+  let n = 1
+  const dir = `./session/${userId}/`
+  const filename = `${assessmentQuizSubId}-${n}.txt`
+  const pathToFile = `${dir}${filename}`
+  const jsonStr = JSON.stringify(data)
+  const compressedData = compressJsonStr(jsonStr)
+  createDir({path: dir, recursive: true})
+  writeToLocal({path: pathToFile, data: compressedData})
+}
