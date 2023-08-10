f4c1dfbba545d1156d42cb87a87fdbda159432be
saymyname-7 <samhoho998@gmail.com>
2023-08-10 10:43:17 +0800

    rm unwanted functions

--- src/lib/core/analytic.ts
+++ src/lib/core/analytic.ts
@@ -2,40 +2,6 @@ import {compressSync, decompressSync, strFromU8, strToU8} from 'fflate'
 import {mkdir, readFile, writeFile} from 'node:fs/promises'
 import {glob} from 'glob'
 
-export const compressJsonStr = (jsonStr: string) => {
-  if (!jsonStr) {
-    throw Error('missing data')
-  }
-  const buf = strToU8(jsonStr)
-  const compressed = compressSync(buf, {level: 6, mem: 8})
-  return compressed
-}
-
-export const decompressBuf = (buf: Uint8Array) => {
-  if (!buf) {
-    throw Error('missing buf')
-  }
-  const decompressed = decompressSync(buf)
-  return JSON.parse(strFromU8(decompressed))
-}
-
-export const createDir = ({
-  path,
-  recursive,
-}: {
-  path: string
-  recursive: boolean
-}) => {
-  if (!path) {
-    throw Error('missing path')
-  } else if (!recursive) {
-    throw Error('missing recursive')
-  }
-  mkdir(path, {recursive}, (err) => {
-    if (err) throw err
-  })
-}
-
 export const writeSessionReplay = async ({
   data,
   userId,
@@ -86,18 +52,4 @@ export const readSessionReplay = async ({
     sessionReplay.push(...json)
   }
   return sessionReplay
-}
-
-export const getLocalFiles = async ({
-  userId,
-  assessmentQuizSubId,
-}: {
-  userId: string
-  assessmentQuizSubId: string
-}) => {
-  if (!userId) {
-    throw Error('missing userId')
-  } else if (!assessmentQuizSubId) {
-    throw Error('missing assessmentQuizSubId')
-  }
 }
