b60a76684ec1155892cfc5f72425e03c78ff7532
saymyname-7 <samhoho998@gmail.com>
2023-08-14 10:57:59 +0800

    add top level describe block

--- tests/assessments-service.integration.test.ts
+++ tests/assessments-service.integration.test.ts
@@ -6,55 +6,57 @@ const prisma = new PrismaClient()
 const uuid = faker.string.uuid()
 const word = faker.lorem.word()
 
-describe('Integration test: createAssessmentService', () => {
-  beforeAll(async () => {
-    await prisma.user.create({data: {id: uuid, name: word}})
-    await prisma.codeLanguage.create({data: {id: 1, name: word}})
-    await prisma.difficultyLevel.create({data: {id: 1, name: word}})
-    await prisma.quiz.create({
-      data: {
-        id: uuid,
-        title: word,
+describe('Integration test: Assessment', () => {
+  describe('Integration test: createAssessmentService', () => {
+    beforeAll(async () => {
+      await prisma.user.create({data: {id: uuid, name: word}})
+      await prisma.codeLanguage.create({data: {id: 1, name: word}})
+      await prisma.difficultyLevel.create({data: {id: 1, name: word}})
+      await prisma.quiz.create({
+        data: {
+          id: uuid,
+          title: word,
+          userId: uuid,
+          codeLanguageId: 1,
+          difficultyLevelId: 1,
+        },
+      })
+    })
+
+    afterAll(async () => {
+      await prisma.assessmentQuiz.deleteMany()
+      await prisma.assessment.deleteMany()
+      await prisma.quiz.deleteMany()
+      await prisma.codeLanguage.deleteMany()
+      await prisma.difficultyLevel.deleteMany()
+      await prisma.user.deleteMany()
+    })
+
+    test('it should create a new assessment', async () => {
+      const assessment = await createAssessmentService({
         userId: uuid,
-        codeLanguageId: 1,
-        difficultyLevelId: 1,
-      },
-    })
-  })
+        title: word,
+        description: word,
+        quizIds: [uuid],
+      })
 
-  afterAll(async () => {
-    await prisma.assessmentQuiz.deleteMany()
-    await prisma.assessment.deleteMany()
-    await prisma.quiz.deleteMany()
-    await prisma.codeLanguage.deleteMany()
-    await prisma.difficultyLevel.deleteMany()
-    await prisma.user.deleteMany()
-  })
-
-  test('it should create a new assessment', async () => {
-    const assessment = await createAssessmentService({
-      userId: uuid,
-      title: word,
-      description: word,
-      quizIds: [uuid],
-    })
-
-    expect(
-      await prisma.assessment.findUnique({
-        where: {id: assessment.id},
-        include: {
-          assessmentCandidateEmail: true,
-          owner: {
-            select: {
-              adminContact: {
-                select: {
-                  companyName: true,
+      expect(
+        await prisma.assessment.findUnique({
+          where: {id: assessment.id},
+          include: {
+            assessmentCandidateEmail: true,
+            owner: {
+              select: {
+                adminContact: {
+                  select: {
+                    companyName: true,
+                  },
                 },
               },
             },
           },
-        },
-      }),
-    ).toEqual(assessment)
+        }),
+      ).toEqual(assessment)
+    })
   })
 })
