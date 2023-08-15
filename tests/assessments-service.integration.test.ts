d89ae3147f65c08fe656f06f6ded088be2e8c040
saymyname-7 <samhoho998@gmail.com>
2023-08-15 17:54:38 +0800

    add getAssessmentsService test cases

--- tests/assessments-service.integration.test.ts
+++ tests/assessments-service.integration.test.ts
@@ -1,4 +1,7 @@
-import {createAssessmentService} from '@/lib/core/service'
+import {
+  createAssessmentService,
+  getAssessmentsService,
+} from '@/lib/core/service'
 import prisma from '@/lib/db/client'
 import {faker} from '@faker-js/faker'
 
@@ -115,6 +118,69 @@ describe('Integration test: Assessment', () => {
       }
       expect(async () => await createAssessmentService(param)).rejects.toThrow(
         /^empty quizIds$/,
+      )
+    })
+  })
+
+  describe('Integration test: createAssessmentService', () => {
+    let assessments: Record<string, any> = {}
+
+    beforeAll(async () => {
+      await prisma.user.create({data: {id: uuid, name: word}})
+      assessments = await prisma.assessment.createMany({
+        data: [
+          {id: uuid + 1, title: word, description: word, ownerId: uuid},
+          {id: uuid + 2, title: word, description: word, ownerId: uuid},
+        ],
+      })
+    })
+
+    afterAll(async () => {
+      await prisma.assessment.deleteMany()
+      await prisma.user.deleteMany()
+    })
+
+    test('empty quizIds param should return an empty quizIds error', async () => {
+      const expectedAssessments: any = []
+
+      for (let i = 1; i <= assessments.count; i++) {
+        expectedAssessments.push(
+          await prisma.assessment.findUnique({
+            where: {id: uuid + i},
+            select: {
+              id: true,
+              title: true,
+              assessmentQuizzes: {
+                select: {
+                  quizId: true,
+                },
+              },
+              assessmentCandidates: {
+                select: {
+                  status: true,
+                  candidate: {
+                    select: {
+                      name: true,
+                    },
+                  },
+                },
+              },
+              createdAt: true,
+              description: true,
+            },
+          }),
+        )
+      }
+
+      expect(await getAssessmentsService({userId: uuid})).toEqual(
+        expectedAssessments,
+      )
+    })
+
+    test('missing userId param should return an empty quizIds error', async () => {
+      const param: any = {}
+      expect(async () => await getAssessmentsService(param)).rejects.toThrow(
+        /^missing userId$/,
       )
     })
   })
