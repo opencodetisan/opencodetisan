2f4283bd9e7a5f4169cfdcccc62a3bf11c149c4e
saymyname-7 <samhoho998@gmail.com>
2023-08-25 15:28:52 +0800

    add getCandidatePointLevel

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import {CandidatePointLevel} from '@/enums'
+
 export const convertToMinuteSecond = (seconds: number) => {
   if (!seconds) {
     throw Error('missing seconds')
@@ -5,4 +7,14 @@ export const convertToMinuteSecond = (seconds: number) => {
   const minutes = Math.floor(seconds / 60)
   const newSeconds = Math.round(seconds - minutes * 60)
   return {minutes, seconds: newSeconds}
+}
+
+export const getCandidatePointLevel = (value: number) => {
+  if (value < 49) {
+    return CandidatePointLevel.Low
+  } else if (value < 79) {
+    return CandidatePointLevel.Medium
+  } else {
+    return CandidatePointLevel.High
+  }
 }
