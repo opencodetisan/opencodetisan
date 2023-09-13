7d258649558eb13f4c2258bcba4665818c786a31
saymyname-7 <samhoho998@gmail.com>
2023-09-13 14:44:57 +0800

    add main-header ui

---
+++ src/components/ui/main-header.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import {cn} from '@/lib/utils'
+import Link from 'next/link'
+import {LayersIcon} from '@radix-ui/react-icons'
+import {Avatar, AvatarFallback, AvatarImage} from './avatar'
+
+export function MainHeader({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <div
+      className={cn(
+        'h-full flex justify-between items-center space-x-4 lg:space-x-6',
+        className,
+      )}
+      {...props}
+    >
+      <LayersIcon className='w-6 h-6' />
+      <Avatar>
+        <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  )
+}
