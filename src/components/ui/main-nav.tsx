94b20a81ad9a96cb5fd5be585b4ccd0926a56fe1
saymyname-7 <samhoho998@gmail.com>
2023-09-14 12:32:50 +0800

    refactor main-nav ui component

--- src/components/ui/main-nav.tsx
+++ src/components/ui/main-nav.tsx
@@ -2,45 +2,46 @@
 
 import {cn} from '@/lib/utils'
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <nav
-      className={cn(
-        'h-full flex items-center space-x-4 lg:space-x-6',
-        className,
-      )}
-      {...props}
-    >
+  const navLinks = [
+    {name: 'Dashboard', href: ''},
+    {name: 'Assessments', href: ''},
+    {name: 'Quizzes', href: '/a/quizzes'},
+    {name: 'Users', href: ''},
+    {name: 'Settings', href: ''},
+  ]
+
+  const components = navLinks.map((link) => {
+    const pathname = usePathname()
+    const isActive = pathname === link.href
+
+    return (
       <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium transition-colors hover:text-primary h-full flex items-center border-b-2 border-black'
+        href={link.href}
+        className={`relative text-sm font-normal transition-colors h-full flex items-center ${
+          isActive ? 'text-primary' : 'text-gray-500'
+        }`}
       >
-        <p className='hover:bg-stone-200 px-3 py-1 my-2 rounded-md'>
-          Dashboard
+        <p className='hover:bg-zinc-200 hover:text-primary px-3 py-1 my-2 rounded-md'>
+          {link.name}
         </p>
+        <div
+          className={`absolute bottom-0 left-4 right-4 border-b-2 border-black transform translate-y-0.5 ${
+            isActive ? '' : 'hidden'
+          }`}
+        ></div>
       </Link>
-      <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
-      >
-        Assessment
-      </Link>
-      <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
-      >
-        Quiz
-      </Link>
-      <Link
-        href='/examples/dashboard'
-        className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'
-      >
-        Settings
-      </Link>
+    )
+  })
+
+  return (
+    <nav className={cn('h-full flex items-center ', className)} {...props}>
+      {components}
     </nav>
   )
 }
