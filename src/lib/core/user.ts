0a28eef2d1e2e36755d4e078d09632b655547494
saymyname-7 <samhoho998@gmail.com>
2023-09-18 16:03:10 +0800

    add getUserForAuth fn

---
+++ src/lib/core/user.ts
@@ -0,0 +1,22 @@
+import prisma from '../db/client'
+
+export const getUserForAuth = async ({email}: {email: string}) => {
+  if (!email) {
+    throw Error('missing email')
+  }
+  const user = await prisma.user.findUnique({
+    where: {email},
+    select: {
+      id: true,
+      email: true,
+      role: true,
+      name: true,
+      userKey: {
+        select: {
+          hashedPassword: true,
+        },
+      },
+    },
+  })
+  return user
+}
