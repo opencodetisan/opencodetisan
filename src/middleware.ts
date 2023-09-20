baa5b4a2644f950d41a78259cc1caace65efdd23
saymyname-7 <samhoho998@gmail.com>
2023-09-20 12:12:15 +0800

    update middleware redirect logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import {withAuth} from 'next-auth/middleware'
 import {NextResponse} from 'next/server'
 import {UserRole} from './enums'
+import {getRoleURLSegment} from './lib/utils'
 
 interface IUserProps {
   name: string
@@ -13,11 +14,16 @@ export default withAuth(
     const pathname = req.nextUrl.pathname
     const user = req.nextauth.token?.user as IUserProps
     const role = user.role
+    const roleURLSegment = getRoleURLSegment(role)
 
     if (pathname.startsWith('/a') && role !== UserRole.Admin) {
-      return NextResponse.rewrite(new URL('/', req.url))
+      return NextResponse.redirect(
+        new URL(roleURLSegment, process.env.NEXTAUTH_URL),
+      )
     } else if (pathname.startsWith('/u') && role === UserRole.Candidate) {
-      return NextResponse.rewrite(new URL('/', req.url))
+      return NextResponse.redirect(
+        new URL(roleURLSegment, process.env.NEXTAUTH_URL),
+      )
     }
 
     const response = NextResponse.next()
