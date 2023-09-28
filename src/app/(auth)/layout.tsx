7156fe2c8a718258a80010681d089c1e96047d1b
saymyname-7 <samhoho998@gmail.com>
2023-09-28 10:04:28 +0800

    modify (auth) root layout add NextAuth provider + Toaster + styling

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,20 @@
+import {Toaster} from '@/components/ui/toaster'
+import '../globals.css'
+import NextAuthProvider from '../nextauth-provider'
+
 export default function SignInLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <NextAuthProvider>
+          <div className='min-h-screen flex justify-center items-center'>
+            <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+              {children}
+            </div>
+          </div>
+          <Toaster />
+        </NextAuthProvider>
+      </body>
     </html>
   )
 }
