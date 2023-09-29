27a05a819c15a6b687f1957568c55302106bb2c1
saymyname-7 <samhoho998@gmail.com>
2023-09-29 11:06:28 +0800

    code cleanup

--- src/app/api/get-many-quizzes/route.ts
+++ src/app/api/get-many-quizzes/route.ts
@@ -2,13 +2,6 @@ import prisma from '@/lib/db/client'
 import {NextResponse} from 'next/server'
 
 export async function GET() {
-  // const res = await fetch('https://data.mongodb-api.com/...', {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'API-Key': process.env.DATA_API_KEY,
-  //   },
-  // })
-  // const data = await res.json()
   const quizzes = await prisma.quiz.findMany()
 
   return NextResponse.json(quizzes)
