07fdf45812f68864183d7506e1054596eb46f409
saymyname-7 <samhoho998@gmail.com>
2023-10-02 11:32:21 +0800

    dashboard rootLayout add white bg

--- src/app/(dashboard)/a/quizzes/layout.tsx
+++ src/app/(dashboard)/a/quizzes/layout.tsx
@@ -3,7 +3,7 @@ import {Separator} from '@/components/ui/separator'
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <div className=''>
-      <div className='py-12 mx-72'>
+      <div className='px-72 py-12 bg-white'>
         <h2 className='text-3xl tracking-tight'>Quizzes</h2>
       </div>
       <Separator />
