b325590b0b1b5c144287c4ec78cfab613a5a8abf
saymyname-7 <samhoho998@gmail.com>
2023-10-04 14:58:13 +0800

    accept showAll url param to query quizzes

--- src/app/api/get-many-quizzes/route.ts
+++ src/app/api/get-many-quizzes/route.ts
@@ -1,8 +1,17 @@
-import prisma from '@/lib/db/client'
+import {getManyQuizService, getQuizService} from '@/lib/core/service'
+import {getServerSession} from 'next-auth'
 import {NextResponse} from 'next/server'
+import {authOptions} from '../auth/[...nextauth]/route'
 
-export async function GET() {
-  const quizzes = await prisma.quiz.findMany()
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions)
+  const userId: string | undefined = session?.user.id
+  const params = new URL(request.url).searchParams
+  const showAll = params.get('showAll') === 'true'
+
+  const quizzes = await getManyQuizService({
+    userId: showAll ? undefined : userId,
+  })
 
   return NextResponse.json(quizzes)
 }
