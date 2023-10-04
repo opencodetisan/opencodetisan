15447385f4fe3e59e1f8813d9f0880ffad955903
saymyname-7 <samhoho998@gmail.com>
2023-10-04 15:06:30 +0800

    rename component

--- src/app/(dashboard)/a/quizzes/page.tsx
+++ src/app/(dashboard)/a/quizzes/page.tsx
@@ -3,7 +3,7 @@ import {RedirectButton} from '@/components/ui/redirect-button'
 import {Separator} from '@/components/ui/separator'
 import {ManyQuizCard} from './component/many-quiz-card'
 
-export default function Home() {
+export default function Quizzes() {
   return (
     <>
       <div className='flex justify-between px-72 py-12 bg-white'>
