4490288ede2a1bbc2fab49b65dd7fcac11023370
saymyname-7 <samhoho998@gmail.com>
2023-10-04 15:01:02 +0800

    mv header to page.tsx

--- src/app/(dashboard)/a/quizzes/layout.tsx
+++ src/app/(dashboard)/a/quizzes/layout.tsx
@@ -1,13 +1,3 @@
-import {Separator} from '@/components/ui/separator'
-
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  return (
-    <div className=''>
-      <div className='px-72 py-12 bg-white'>
-        <h2 className='text-3xl tracking-tight'>Quizzes</h2>
-      </div>
-      <Separator />
-      <div className='px-72 pt-6'>{children}</div>
-    </div>
-  )
+  return <div className=''>{children}</div>
 }
