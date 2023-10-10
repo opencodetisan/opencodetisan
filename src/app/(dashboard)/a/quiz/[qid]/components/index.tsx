3e20f6871172edcc6d0af8af7a2434838dfcf9c1
saymyname-7 <samhoho998@gmail.com>
2023-10-10 12:32:16 +0800

    reuse quiz basic-config form UI component

--- src/app/(dashboard)/a/quiz/[qid]/components/index.tsx
+++ src/app/(dashboard)/a/quiz/[qid]/components/index.tsx
@@ -218,7 +218,10 @@ export default function MainComponent({
   )
 }
 
-function BasicConfigurationDialog({children}: any) {
+function BasicConfigurationDialog({children, handleSubmit}: any) {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -229,22 +232,16 @@ function BasicConfigurationDialog({children}: any) {
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className='grid gap-4 py-4'>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='name' className='text-right'>
-              Name
-            </Label>
-            <Input id='name' value='Pedro Duarte' className='col-span-3' />
-          </div>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='username' className='text-right'>
-              Username
-            </Label>
-            <Input id='username' value='@peduarte' className='col-span-3' />
-          </div>
-        </div>
+        <QuizDetails />
         <DialogFooter>
-          <Button type='submit'>Save changes</Button>
+          <Button
+            type='submit'
+            onClick={() => {
+              handleSubmit(onSubmit)()
+            }}
+          >
+            Save changes
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
