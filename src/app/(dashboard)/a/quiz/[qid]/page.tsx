f2ab2c980b47e1620d07ea71b8f84ecb1cda972b
saymyname-7 <samhoho998@gmail.com>
2023-10-12 17:59:18 +0800

    fix invalid props import

--- src/app/(dashboard)/a/quiz/[qid]/page.tsx
+++ src/app/(dashboard)/a/quiz/[qid]/page.tsx
@@ -1,12 +1,8 @@
 import MainComponent from './components'
 
-export default function Quiz({
-  className,
-  title,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
+export default function Quiz({}: any) {
   return (
-    <div className={`${className} flex justify-center pt-6`}>
+    <div className={`flex justify-center pt-6`}>
       <MainComponent />
     </div>
   )
