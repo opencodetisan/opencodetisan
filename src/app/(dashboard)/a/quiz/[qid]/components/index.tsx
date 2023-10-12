83a2b6fa8f21c148e610a2faf40f89bea7183e07
saymyname-7 <samhoho998@gmail.com>
2023-10-12 16:34:51 +0800

    code cleanup

--- src/app/(dashboard)/a/quiz/[qid]/components/index.tsx
+++ src/app/(dashboard)/a/quiz/[qid]/components/index.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import {Button} from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import {Card, CardContent, CardFooter, CardHeader} from '@/components/ui/card'
 import {CodeEditor} from '@/components/ui/code-editor'
 import {
   Dialog,
@@ -20,7 +13,6 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import {Input} from '@/components/ui/input'
-import {Label} from '@/components/ui/label'
 import {Separator} from '@/components/ui/separator'
 import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/tabs'
 import {fetcher} from '@/lib/fetcher'
