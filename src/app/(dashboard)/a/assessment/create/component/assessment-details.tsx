f59b243025dfca3a6434667a596a88504e962d5b
saymyname-7 <samhoho998@gmail.com>
2023-10-13 14:14:25 +0800

    setup assessment-details

---
+++ src/app/(dashboard)/a/assessment/create/component/assessment-details.tsx
@@ -0,0 +1,56 @@
+import {Card, CardHeader} from '@/components/ui/card'
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import {Input} from '@/components/ui/input'
+import {useFormContext} from 'react-hook-form'
+
+export function AssessmentDetails({
+  className,
+  isLoading,
+  ...props
+}: {
+  className?: React.HTMLAttributes<HTMLElement>
+  isLoading: boolean
+}) {
+  const form = useFormContext()
+
+  return (
+    <Card>
+      <CardHeader className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='title'
+          disabled={isLoading}
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='description'
+          disabled={isLoading}
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </CardHeader>
+    </Card>
+  )
+}
