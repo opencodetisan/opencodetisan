161fabf46c4240a7034bdb85b203989493dab66b
saymyname-7 <samhoho998@gmail.com>
2023-10-19 00:18:06 +0800

    setup parent component for assessment card

---
+++ src/app/(dashboard)/a/assessments/component/many-assessment-card.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import {fetcher} from '@/lib/fetcher'
+import useSWR from 'swr'
+import {AssessmentCard} from './assessment-card'
+
+export function ManyAssessmentCard({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  const {data} = useSWR(
+    `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/get-many-assessments`,
+    fetcher,
+  )
+
+  let content = <></>
+
+  // TODO: type
+  content = data?.map((assessment: any) => {
+    return (
+      <AssessmentCard
+        key={assessment.id}
+        title={assessment.title}
+        description={assessment.description}
+        owner={assessment.owner.name}
+        candidateAmount={assessment.assessmentCandidates.length}
+        assessmentCandidates={assessment.assessmentCandidates}
+        createdat={assessment.createdAt}
+      />
+    )
+  })
+
+  return (
+    <div className='space-y-5'>
+      <div className={`${className} grid grid-cols-3 gap-4`} {...props}>
+        {content}
+      </div>
+    </div>
+  )
+}
+
+// <div className='flex justify-end'>
+//   <Select
+//     onValueChange={(value: 'false' | 'true') => {
+//       setShowAll(value)
+//     }}
+//   >
+//     <SelectTrigger className='w-[180px]'>
+//       <SelectValue placeholder='Filter' />
+//     </SelectTrigger>
+//     <SelectContent>
+//       <SelectItem value='false'>Show my quizzes</SelectItem>
+//       <SelectItem value='true'>Show all</SelectItem>
+//     </SelectContent>
+//   </Select>
+// </div>
