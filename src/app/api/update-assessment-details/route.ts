77af147e5b414e3dc24609b12f835232802e0992
saymyname-7 <samhoho998@gmail.com>
2023-10-19 23:09:37 +0800

    setup route handler to update assessment details

---
+++ src/app/api/update-assessment-details/route.ts
@@ -0,0 +1,19 @@
+import {updateAssessmentDataService} from '@/lib/core/service'
+import {NextResponse} from 'next/server'
+
+export async function POST(request: Request) {
+  try {
+    const {title, description, id} = await request.json()
+
+    const assessment = await updateAssessmentDataService({
+      title,
+      description,
+      assessmentId: id,
+    })
+
+    return NextResponse.json({})
+  } catch (error) {
+    console.log('Unexpected error: ', error)
+    return NextResponse.json({})
+  }
+}
