4d40a8a2fc0b86de1f73f7fed2a25b311f007418
saymyname-7 <samhoho998@gmail.com>
2023-10-21 16:05:28 +0800

    setup sendUserCredential & sendAssessmentInvitation

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -31,3 +31,77 @@ export const sendPassRecoveryMail = async ({recipient, token}: any) => {
   const result = await transporter.sendMail(message)
   return result
 }
+
+export const sendAssessmentInvitation = async ({
+  recipient,
+  aid,
+}: {
+  recipient: string
+  aid: string
+}) => {
+  const link = `${process.env.NEXTAUTH_URL}/c/assessment/${aid}`
+  const html = `
+      <p>Someone from Mars has invited you to an assessment.</p>
+      <div>
+        <a href="${link}">Click here</a> to recover your password.
+      </div>
+  `
+
+  const message = {
+    from: process.env.NODEMAILER_USERNAME,
+    to: recipient,
+    subject: 'OpenCodetisan assessment invitation',
+    text: 'message',
+    html,
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USERNAME,
+      pass: process.env.NODEMAILER_PASSWORD,
+    },
+  })
+
+  const result = await transporter.sendMail(message)
+  return result
+}
+
+export const sendUserCredential = async ({
+  recipient,
+  aid,
+}: {
+  recipient: string
+  aid: string
+}) => {
+  const link = `${process.env.NEXTAUTH_URL}/c/assessment/${aid}`
+  const html = `
+      <p>Someone from Mars has invited you to an assessment.</p>
+      <div>
+        <a href="${link}">Click here</a> to recover your password.
+      </div>
+  `
+
+  const message = {
+    from: process.env.NODEMAILER_USERNAME,
+    to: recipient,
+    subject: 'OpenCodetisan assessment invitation',
+    text: 'message',
+    html,
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USERNAME,
+      pass: process.env.NODEMAILER_PASSWORD,
+    },
+  })
+
+  const result = await transporter.sendMail(message)
+  return result
+}
