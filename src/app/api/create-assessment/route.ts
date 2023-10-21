cce77cc23e8c61229b86c8f001ef4a58ac170401
saymyname-7 <samhoho998@gmail.com>
2023-10-21 21:33:50 +0800

    setup email sending logics when creating assessment

--- src/app/api/create-assessment/route.ts
+++ src/app/api/create-assessment/route.ts
@@ -1,7 +1,14 @@
 import {getServerSession} from 'next-auth'
 import {NextResponse} from 'next/server'
 import {authOptions} from '../auth/[...nextauth]/route'
-import {createAssessmentService} from '@/lib/core/service'
+import {
+  acceptAssessmentService,
+  createAssessmentService,
+} from '@/lib/core/service'
+import prisma from '@/lib/db/client'
+import {sendAssessmentInvitation, sendUserCredential} from '@/lib/nodemailer'
+import {faker} from '@faker-js/faker'
+import bcrypt from 'bcrypt'
 
 export async function POST(request: Request) {
   try {
@@ -17,6 +24,59 @@ export async function POST(request: Request) {
       quizIds,
       userId,
     })
+
+    // TODO: use service function
+    const userPromises = []
+    const existingCandidatesObj = {}
+    const existingCandidates = await prisma.user.findMany({
+      where: {
+        email: {
+          in: candidates,
+        },
+      },
+      select: {
+        email: true,
+        id: true,
+      },
+    })
+
+    existingCandidates.forEach((c) => (existingCandidatesObj[c.email] = c))
+
+    for (let i = 0; i < candidates.length; i++) {
+      const email = candidates[i]
+
+      if (existingCandidatesObj[email]) {
+        const candidateId = existingCandidatesObj[email].id
+        await acceptAssessmentService({
+          assessmentId: assessment.id,
+          userId: candidateId,
+          token: email + faker.lorem.text(),
+        })
+        await sendAssessmentInvitation({recipient: email, aid: assessment.id})
+      } else {
+        const name = email.split('@')[0]
+        const password = faker.lorem.word({strategy: 'longest'})
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newCandidate = await prisma.user.create({
+          data: {
+            email: email,
+            name,
+            userKey: {
+              create: {
+                password: hashedPassword,
+              },
+            },
+          },
+        })
+        await acceptAssessmentService({
+          assessmentId: assessment.id,
+          userId: newCandidate.id,
+          token: email,
+        })
+        await sendAssessmentInvitation({recipient: email, aid: assessment.id})
+        await sendUserCredential({recipient: email, password})
+      }
+    }
 
     return NextResponse.json({})
   } catch (error) {
