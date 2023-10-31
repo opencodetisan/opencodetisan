8ccffba70509497a2a9ee6d79e39040ec15e983c
saymyname-7 <samhoho998@gmail.com>
2023-10-31 14:27:36 +0800

    import getAllCandidate

--- src/lib/core/service.ts
+++ src/lib/core/service.ts
@@ -19,6 +19,7 @@ import {
   deleteManyAssessmentQuizSubmission,
   deleteManyAssessmentResult,
   getAllAssessmentCandidate,
+  getAllCandidate,
   getAssessment,
   getAssessmentIds,
   getAssessmentQuizSubmission,
