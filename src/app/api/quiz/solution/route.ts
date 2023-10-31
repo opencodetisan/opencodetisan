7856807efafea599298da57de16242b17f7dad8f
saymyname-7 <samhoho998@gmail.com>
2023-10-31 17:06:56 +0800

    use /api/quiz/solution to update quiz solution

--- src/app/api/quiz/solution/route.ts
+++ src/app/api/quiz/solution/route.ts
@@ -1,7 +1,7 @@
 import {updateQuizSolutionService} from '@/lib/core/service'
 import {getServerSession} from 'next-auth'
 import {NextResponse} from 'next/server'
-import {authOptions} from '../auth/[...nextauth]/route'
+import {authOptions} from '../../auth/[...nextauth]/route'
 
 export async function POST(request: Request) {
   try {
