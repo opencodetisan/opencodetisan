259fc0d3706c5b021e485f6448963d7185ea300c
saymyname-7 <samhoho998@gmail.com>
2023-11-06 18:43:22 +0800

    modify nextjs compiler for dockerode to work

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const os = require('node:os')
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, {dev, isServer, webpack, nextRuntime}) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: [
+        {
+          loader: 'nextjs-node-loader',
+          options: {
+            flags: os.constants.dlopen.RTLD_NOW,
+            outputPath: config.output.path,
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
