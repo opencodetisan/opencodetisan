600796ea1613b2bb3efaec14b19d1d74bcc6cbea
saymyname-7 <samhoho998@gmail.com>
2023-11-27 19:01:58 +0800

    add RUN LOADING ERROR constants

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,2 +1,5 @@
 export const QUIZ_COMPLETION_POINT = 'QuizCompletionPoint'
 export const MAX_SPEED_POINT_MULTIPLIER = 1.2
+export const RUN = 'run'
+export const LOADING = 'loading'
+export const ERROR = 'error'
